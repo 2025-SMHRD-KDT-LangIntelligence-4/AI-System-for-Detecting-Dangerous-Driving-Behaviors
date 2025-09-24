1beca8baa9afcc3b87c5489635ff4e33760340d6
smart <smart@smhrd>
2025-09-24 11:32:06 +0900

    프로젝트 다시 만듬 Zero_Accident

---
+++ Zero_Accident/src/main/java/com/smhrd/web/ZeroAccidentApplication.java
@@ -0,0 +1,13 @@
+package com.smhrd.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeroAccidentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeroAccidentApplication.class, args);
+	}
+
+}
